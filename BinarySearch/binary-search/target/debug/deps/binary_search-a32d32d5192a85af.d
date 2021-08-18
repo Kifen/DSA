9ad887e2e23ecd02a7601027ca05fefd9905c383
kifen <kifenraph@gmail.com>
2021-08-18 20:25:25 +0100

    Update .gitignore

--- BinarySearch/binary-search/target/debug/deps/binary_search-a32d32d5192a85af.d
+++
@@ -1,7 +0,0 @@
-/Users/kifen/Dev/misc/DSA/BinarySearch/binary-search/target/debug/deps/binary_search-a32d32d5192a85af.rmeta: src/lib.rs
-
-/Users/kifen/Dev/misc/DSA/BinarySearch/binary-search/target/debug/deps/libbinary_search-a32d32d5192a85af.rlib: src/lib.rs
-
-/Users/kifen/Dev/misc/DSA/BinarySearch/binary-search/target/debug/deps/binary_search-a32d32d5192a85af.d: src/lib.rs
-
-src/lib.rs:
