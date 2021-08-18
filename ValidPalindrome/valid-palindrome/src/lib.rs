13b15adaf297f838249e91a349cd8956f5bb0253
kifen <kifenraph@gmail.com>
2021-08-18 19:49:28 +0100

    Implement test for rust valid-palindrome

---
+++ ValidPalindrome/valid-palindrome/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn valid_palindrome(s: String) -> bool {
+  let s_bytes = s.as_bytes();
+  let mut left = 0;
+  let mut right = s.len() - 1;
+
+  while left < right {
+    if s_bytes[left] != s_bytes[right] {
+      return false;
+    }
+
+    left += 1;
+    right -= 1;
+  }
+
+  return true;
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+    #[test]
+    fn it_works() {
+      let palindrome = "rotator";
+
+        assert_eq!(valid_palindrome(palindrome.to_string()), true);
+    }
+}
---
+++ ValidPalindrome/valid-palindrome/target/debug/deps/valid_palindrome-e5b5fea8022e3d3b.d
@@ -0,0 +1,5 @@
+/Users/kifen/Dev/misc/DSA/ValidPalindrome/valid-palindrome/target/debug/deps/valid_palindrome-e5b5fea8022e3d3b: src/lib.rs
+
+/Users/kifen/Dev/misc/DSA/ValidPalindrome/valid-palindrome/target/debug/deps/valid_palindrome-e5b5fea8022e3d3b.d: src/lib.rs
+
+src/lib.rs:
